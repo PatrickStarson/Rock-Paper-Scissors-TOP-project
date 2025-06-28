f65c4254b1892e9bfdad1114d3099404e8b9fecd
PatrickStarson <spartak.mamiev@gmail.com>
2025-06-28 14:42:24 +0200

    Create a function getHumanChoice and make it return the players choice

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
   //CREATE a variable randomNumber that will contain the random number
   
+  //CREATE a function getComputerChoice
   //IF the randomNuber is between 0 and 3,33 make the function return a string 'Rock'  
   //ELSE IF the randomNuber is between 3.33 and 6,66 make the function return a string 'Paper'
   //ELSE make the function return a string 'Scissors'
 
 const randomNumber = Math.random();
+
 function getComputerChoice() {
   if (randomNumber < 0.33) {
-    return 'Rock';
+    return 'rock';
   } else if (randomNumber > 0.33 && randomNumber <= 0.66) {
-    return 'Paper';
+    return 'paper';
   } else {
-    return 'Scissors';
+    return 'scissors';
     }
 };
 
-console.log(randomNumber);
 console.log(getComputerChoice());
+
+
+//CREATE a function getHumanChoice.
+//CREATE a varialble humanChoice an assign it a prompt where the player can input he's/she's choice.
+//RETURN whatever the player has chosen.
+function getHumanChoice () {
+  const humanChoice = prompt('Choose your move');
+  return humanChoice
+  console.log(humanChoice())
+}
+
+console.log(getHumanChoice())
