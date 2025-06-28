b00ff028335a8d2a9f0f32c726a4f14273cc7f94
PatrickStarson <spartak.mamiev@gmail.com>
2025-06-28 19:42:44 +0200

    Move th randomNumber variable into the getComputerChoice function.Create playRound function and define parameters for human and computer choice. Create variables for human and computer score in the global scope.

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-
-
   let humanScore = 0;
   let computerScore = 0;
-  const randomNumber = Math.random();
-
-    //CREATE a variable randomNumber in global scope, that will contain the random number.
   
   //CREATE a function getComputerChoice
+  //DECLAIR a variable randomNumber and assign it Math.random() method.
   //IF the randomNuber is between 0 and 3,33 make the function return a string 'Rock'  
   //ELSE IF the randomNuber is between 3.33 and 6,66 make the function return a string 'Paper'
   //ELSE make the function return a string 'Scissors'
 
 function getComputerChoice() {
+  const randomNumber = Math.random();
   if (randomNumber < 0.33) {
     return 'rock';
   } else if (randomNumber > 0.33 && randomNumber <= 0.66) {
@@ -19,23 +16,29 @@ function getComputerChoice() {
   } else {
     return 'scissors';
     }
+   
 };
 
 console.log(getComputerChoice());
 
 
+
+
 //CREATE a function getHumanChoice.
-//CREATE a varialble humanChoice an assign it a prompt where the player can input he's/she's choice.
-//RETURN whatever the player has chosen.
+//RETURN whatever the player has chosen and typed in the prompt.
 function getHumanChoice () {
-  const humanChoice = prompt('Choose your move');
-  return humanChoice
-  console.log(humanChoice())
+  return prompt('Choose your move').toLowerCase();
 }
 
 console.log(getHumanChoice())
 
-//CREATE variable for humanScore in global scope and give it a value of 0. This will be used to keep score for the human.
-//CREATE variable for computerScore in global scope and give it a value of 0. This will be used to keep score for the human.
+//DECLAIR variable for humanScore in global scope and give it a value of 0. This will be used to keep score for the human.
+//DECLAIR variable for computerScore in global scope and give it a value of 0. This will be used to keep score for the human.
+
+//CREATE a function named playRound. It will compare players and computers choices and determin who winns.
+//
+
+function playRound (humanChoice = getHumanChoice(), computerChoice = getComputerChoice()) {
 
 
+}
