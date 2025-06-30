8877fce0cdf8a4c74aef2956598423981c8c1526
PatrickStarson <spartak.mamiev@gmail.com>
2025-06-30 11:10:23 +0200

    Create a function named playGame and copy entire playRound function and score variables into it. Use for loop to run playRound 5 times by adding it to a for loop. Console.log the winner in the end.

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
+// Step1
 // CREATE a function named getComputerChioce.
 // DECLAIR a variable named randomNumber and asign it a Math.random() method.
 // IF randomNumber is between 0-0,33 return a string 'rock'.
 // ELSE IF randomNumber is between 0,33-0,66 return a string  'paper'.
 // ELSE return a string 'scissors'.
 // CONSOLE.LOG the getComputerChoice result to the console to make sure it works
+
+//Step2
+// CREATE a function named getHumanChoice
+// CREATE a prompt method to get user's input
+// RETURN what the user enters into the prompt
+
+//Step 3
+// DECLAIR to variables humanScore and computerScore, both with value of 0.
+
+//Step4
+// CREATE a functions named playRound with parameters humanChoice and computerChoice.
+// DECLAIRE variables humanSelection and computerSelection after the playRound function and asign getHumanChoice and getComputerChoice to them.
+// PASS humanSelection and computerSelection as arguments to playRound function.
+// MAKE humanChoice input case-insensitive by adding toLowerCase() method. You can add the method to the prompt in getHumanChoice function.
+// IF humanChoice es equal to computerChoice log a string "It's a Tie!" in the console.
+// ELSE IF humanChoice beats computerChoice log a string `You win! ${humanChoice} beats ${computerChoice}.` in the console and increment humanScore by 1.
+// ELSE IF computerChoice beats humanChoice log a string `You lose! ${computerChoice} beats ${humanChoice}.` in the console and increment computerScore by 1.
+
+//Step 5
+// CREATE a function named playGame.
+// MOVE playRound function and score variables into the playGame function.
+// WRITE a for loop that will run playRound function 5 times.
 
 function getComputerChioce() {
   const randomNumber = Math.random();
@@ -17,34 +40,19 @@ function getComputerChioce() {
 };
 
 
-
-
-// CREATE a function named getHumanChoice
-// CREATE a prompt method to get user's input
-// RETURN what the user enters into the prompt
-
 function getHumanChoice() {
   return prompt().toLowerCase();
 };
 
 
+function playGame () {
+  
+  let humanScore = 0;
+  let computerScore = 0;
 
-// DECLAIR to variables humanScore and computerScore, both with value of 0.
-
-let humanScore = 0;
-let computerScore = 0;
-
-
-// CREATE a functions named playRound with parameters humanChoice and computerChoice.
-// DECLAIRE variables humanSelection and computerSelection after the playRound function and asign getHumanChoice and getComputerChoice to them.
-// PASS humanSelection and computerSelection as arguments to playRound function.
-// MAKE humanChoice input case-insensitive by adding toLowerCase() method. You can add the method to the prompt in getHumanChoice function.
-// IF humanChoice es equal to computerChoice log a string "It's a Tie!" in the console.
-// ELSE IF humanChoice beats computerChoice log a string `You win! ${humanChoice} beats ${computerChoice}.` in the console and increment humanScore by 1.
-// ELSE IF computerChoice beats humanChoice log a string `You lose! ${computerChoice} beats ${humanChoice}.` in the console and increment computerScore by 1.
-
-
-function playRound (humanChoice, computerChoice) {
+  for(let i = 0; i < 5; i++) {
+    
+    function playRound (humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
     console.log("It's a Tie.")
   } else if (
@@ -60,10 +68,19 @@ function playRound (humanChoice, computerChoice) {
 
   console.log(`Human: ${humanScore}`);
   console.log(`Computer: ${computerScore}`);
-  
 }
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChioce();
 
 playRound(humanSelection, computerSelection)
+};
+
+  if(humanScore > computerScore) {
+  console.log('The Human won!')
+} else {
+  console.log('The Computer won!')
+}
+};
+
+playGame();
